e11cc00eb8e7407381f66e4378a1ecc6a6b9620b
X-MAJ <117451874+xmaj2001@users.noreply.github.com>
2023-06-20 06:50:14 -0300

    O projecto
    O Wolf Login é um projeto que demonstra como criar uma interface de login animada utilizando o WPF, MaterialDesign e o Blend for Visual Studio. Este repositório contém o código-fonte completo e um tutorial passo a passo para ajudá-lo a reproduzir a interface no seu próprio projeto. Aprenda a desenvolver interfaces atraentes e animadas com facilidade usando as tecnologias mencionadas. Comece agora mesmo e impressione seus usuários com uma UI incrível!

---
+++ WolfLoginWhite/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WolfLoginWhite
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            st_Copy1_BeginStoryboard.Storyboard.Completed += Storyboard_Completed;
+        }
+
+        private void Storyboard_Completed(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) DragMove();
+        }
+    }
+}
